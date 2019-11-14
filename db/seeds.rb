76eec4dfdfcf928e0468a42cb3f8a1915c90e474
Todd <todd.bisel@gmail.com>
2019-11-14 12:39:08 -0500

    .

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,3 +20,24 @@ Game.create(
   description: "Dungeons & Dragons is a structured yet open-ended role-playing game. It is normally played indoors with the participants seated around a tabletop. Typically, each player controls only a single character, which represents an individual in a fictional setting.[16] When working together as a group, these player characters (PCs) are often described as a party of adventurers, with each member often having their own area of specialty which contributes to the success of the whole. During the course of play, each player directs the actions of their character and their interactions with other characters in the game. This activity is performed through the verbal impersonation of the characters by the players, while employing a variety of social and other useful cognitive skills, such as logic, basic mathematics and imagination. A game often continues over a series of meetings to complete a single adventure, and longer into a series of related gaming adventures, called a campaign.",
   image: "https://i.pinimg.com/originals/c7/c3/a0/c7c3a029d172b33287003d26a0c693f9.png"
   )
+
+
+
+Game.create(
+  title: "Dungeons & Dragons 2E",
+  description: "Dungeons & Dragons is a structured yet open-ended role-playing game. It is normally played indoors with the participants seated around a tabletop. Typically, each player controls only a single character, which represents an individual in a fictional setting.[16] When working together as a group, these player characters (PCs) are often described as a party of adventurers, with each member often having their own area of specialty which contributes to the success of the whole. During the course of play, each player directs the actions of their character and their interactions with other characters in the game. This activity is performed through the verbal impersonation of the characters by the players, while employing a variety of social and other useful cognitive skills, such as logic, basic mathematics and imagination. A game often continues over a series of meetings to complete a single adventure, and longer into a series of related gaming adventures, called a campaign.",
+  image: "https://thetrove.net/Assets/D&D%20Homebrew/Logos/2nd%20Edition/AD&D%202e.png"
+  )
+
+Game.create(
+  title: "Dungeons & Dragons 3E",
+  description: "Dungeons & Dragons is a structured yet open-ended role-playing game. It is normally played indoors with the participants seated around a tabletop. Typically, each player controls only a single character, which represents an individual in a fictional setting.[16] When working together as a group, these player characters (PCs) are often described as a party of adventurers, with each member often having their own area of specialty which contributes to the success of the whole. During the course of play, each player directs the actions of their character and their interactions with other characters in the game. This activity is performed through the verbal impersonation of the characters by the players, while employing a variety of social and other useful cognitive skills, such as logic, basic mathematics and imagination. A game often continues over a series of meetings to complete a single adventure, and longer into a series of related gaming adventures, called a campaign.",
+  image: "https://wetabletop.s3.amazonaws.com/uploads/game/image/35/1200px-Dungeons___Dragons_5th_Edition_logo.svg.png"
+  )
+
+Game.create(
+  title: "Starfinder",
+  description: "Take your favorite fantasy RPG to the stars! Set thousands of years in Pathfinder's Click this link to see if this article exists on PathfinderWiki. future, Starfinder is a stand-alone roleplaying game evolved from the Pathfinder rules Click this link to see if this article exists on PathfinderWiki. and designed to bring you a whole new universe of science fantasy adventures. Play alien races both new and familiar as you explore the mysteries of a weird galaxy. Will you be an android assassin fulfilling corporate contracts, or a plucky ratfolk mechanic? A spellhacking lashunta technomancer, or a rakish human pilot? Uncountable worlds are waiting for you and your intrepid crew!
+The Starfinder line will include both a hardcover core rulebook and key hardcover rules supplements, as well as a Starfinder Adventure Path that provides you with epic campaigns, expanded rules elements, and new monsters to battle. Best of all, Starfinder is designed to integrate easily with the Pathfinder roleplaying game, meaning your power-armored marine can still go toe to toe with orcs Click this link to see if this article exists on PathfinderWiki. and dragons Click this link to see if this article exists on PathfinderWiki.. So what are you waiting for? The galaxy needs you!",
+  image: "https://cdn.paizo.com/image/content/Secondary/OWD-29024-2_500.jpeg"
+  )
